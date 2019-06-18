b0cd410a29a54fa3e4aff898c420fad0b8d1745d
haidichen <chenhaidi43@163.com>
2019-06-18 14:40:02 -0500

    broadcast files via mqtt

--- mqtt/pub.py
+++ mqtt/pub.py
@@ -1,9 +1,15 @@
 import paho.mqtt.client as mqtt
 
-broker_url = '0.0.0.0'
+broker_url = '192.168.72.133'
 broker_port = 1883
 
 client = mqtt.Client()
 client.connect(broker_url, broker_port)
 
-client.publish('TestingTopic', "Testing payload", 1, False)
+client.publish('text', 'hello, there! Sent an image.', 1, False)
+
+with open('cat.jpg', 'rb') as f:
+    filepayload = f.read()
+    filebyte = bytearray(filepayload)
+
+    client.publish('image', filebyte, 1, False)
