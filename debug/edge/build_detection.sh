373d2114b610ba8fb7f5ac387c27fd06b9a47f07
haidichen <chenhaidi43@163.com>
2019-07-25 09:35:18 -0500

    move the file downloading and generating from Dockerfile to build file

--- debug/edge/build_detection.sh
+++ debug/edge/build_detection.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 cd detection
+
+if [ ! -e 'yolov3.weights' ]
+then
+  echo 'yolov3.weights missing.'
+  wget https://pjreddie.com/media/files/yolov3.weights
+fi
+
+if [ ! -e 'model_data/yolo.h5' ]
+then
+  echo 'model_data/yolo.h5 missing.'
+  python3 convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
+fi
+
 docker build -t thatape/ecf_edge:detection .
 docker build -t st571332/ecf_edge:detection .
