d7cff5c7ce2d6b7abe35d2edd27ba88b5e6eec96
haidichen <chenhaidi43@163.com>
2019-11-12 14:56:35 -0600

    updated grab.sh

--- debug/edge/grab/grab.sh
+++ debug/edge/grab/grab.sh
@@ -4,16 +4,13 @@ cd ../rawdata
 
 # vehicle IP address
 V_IP_File='../vip/ip.txt'
-vehicle_IP=''
 
 while read -r line
 do
-	vehicle_IP="$line"
+    vehicle_IP="$line"
+    wget -r -nd "http://$vehicle_IP:9000"
+    if [ -e 'index.html' ]
+    then
+      rm index.html*
+    fi
 done < "$V_IP_File"
-
-wget -r -nd "http://$vehicle_IP:9000"
-
-if [ -e 'index.html' ]
-then
-	rm index.html
-fi
