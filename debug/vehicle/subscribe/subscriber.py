cbe9322f4a6a011eda4032acf14785eeee9290ad
haidichen <chenhaidi43@163.com>
2019-11-24 19:27:14 -0600

    deleted out-dated code

--- debug/vehicle/subscribe/subscriber.py
+++ debug/vehicle/subscribe/subscriber.py
@@ -10,7 +10,6 @@ class Subscriber(object):
     OUTPUT_FOLDER = '../output/'
 
     def __init__(self, broker):
-        self._filename = 'hello'
         self._client = mqtt.Client()
         self._qos = 0
         self._client.on_connect = self._on_connect
@@ -18,17 +17,6 @@ class Subscriber(object):
 
     def _on_connect(self, client, userdata, flags, rc):
         print('connected with result code', rc)
-
-    def _on_message_filename(self, client, userdata, message):
-        self._filename = message.payload.decode()
-        print('filename reset to ', self._filename)
-
-    def _on_message_result(self, client, userdata, message):
-        with open(os.path.join(Subscriber.OUTPUT_FOLDER, self._filename),
-                  'wb') as f:
-            f.write(message.payload)
-
-        print('file received')
 
     def _on_json_data(self, client, userdata, message):
         decoded = str(message.payload.decode("utf-8", "ignore"))
