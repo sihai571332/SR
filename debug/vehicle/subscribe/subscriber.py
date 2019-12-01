5b5ce0257d0df449ffb1be64092b56b787ec7a0d
haidichen <chenhaidi43@163.com>
2019-11-30 21:57:23 -0600

    refactored the code

--- debug/vehicle/subscribe/subscriber.py
+++ debug/vehicle/subscribe/subscriber.py
@@ -9,36 +9,42 @@ class Subscriber(object):
     # constant variable
     OUTPUT_FOLDER = '../output/'
 
-    def __init__(self, broker):
+    def __init__(self):
         self._client = mqtt.Client()
         self._qos = 0
+        self._message = {}
         self._client.on_connect = self._on_connect
+
+    def connect_broker(self, broker):
         self._client.connect(broker.get_url(), broker.get_port(), 1800)
 
     def _on_connect(self, client, userdata, flags, rc):
         print('connected with result code', rc)
 
-    def _on_json_data(self, client, userdata, message):
+    def _on_receive_data(self, client, userdata, message):
+        self._extract_message(message)
+        self._recover_image()
+        self._print_name_of_received_file()
+    
+    def _recover_image(self):
+        filename = self._message['filename']
+        with open(os.path.join(Subscriber.OUTPUT_FOLDER, filename), 'wb') as f:
+            f.write(base64.b64decode(self._message['imageString']))
+
+    def _print_name_of_received_file(self):
+        print(self._message['filename'])
+
+    def _extract_message(self, message):
         decoded = str(message.payload.decode("utf-8", "ignore"))
-        data = json.loads(decoded)
-        fname = data['filename']
-        print(fname)
-        with open(os.path.join(Subscriber.OUTPUT_FOLDER, fname), 'wb') as f:
-            f.write(base64.b64decode(data['img']))
+        self._message = json.loads(decoded)
 
-    def sub(self, topics):
-        for i in range(0, len(topics)):
-            self._client.subscribe(topics[i], self._qos)
-
-            self._client.message_callback_add(topics[i],
-                    self._on_json_data)
+    def subscribe_to_topics(self, topics):
+        for topic in topics:
+            self._subscribe_to_one_topic(topic)
 
         self._client.loop_forever()
 
-    def sub2(self, topics):
-        self._client.subscribe(topics, self._qos)
-
-        self._client.message_callback_add(topics, self._on_json_data)
-
-        self._client.loop_forever()
+    def _subscribe_to_one_topic(self, topic):
+        self._client.subscribe(topic, self._qos)
+        self._client.message_callback_add(topic, self._on_receive_data)
 
