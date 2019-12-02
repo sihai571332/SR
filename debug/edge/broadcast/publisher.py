1e0c169e9a73952ad468d8237dd85a0fdc7aa58a
haidichen <chenhaidi43@163.com>
2019-12-02 14:37:01 -0600

    refactored the code

--- debug/edge/broadcast/publisher.py
+++ debug/edge/broadcast/publisher.py
@@ -4,10 +4,9 @@ import base64
 import os
 import time
 
-# class for publishing
 class Publisher(object):
 
-    # Constant variables
+    # the folder where to-be-broadcasted files store 
     SOURCE_FOLDER = '../result/'
 
     def __init__(self):
@@ -32,25 +31,32 @@ class Publisher(object):
 
     def _publish_message_on_topics(self, topics):
         self._update_files_in_source_folder()
-
-        for namedFile in self._filesInSourceFolder:
-            self._prepare_message(namedFile)
-            self._attach_to_topics(topics)
+        self._publish_files(topics)
 
     def _update_files_in_source_folder(self):
         self._filesInSourceFolder = os.listdir(Publisher.SOURCE_FOLDER)
 
+    def _publish_files(self, topics):
+        for named_file in self._filesInSourceFolder:
+            self._prepare_message(named_file)
+            self._attach_to_topics(topics)
+
     def _prepare_message(self, filename):
+        self._set_value_of_key_filename(filename)
+        self._set_value_of_key_imageString(filename)
+
+    def _set_value_of_key_filename(self, filename):
         self._message['filename'] = filename
 
+    def _set_value_of_key_imageString(self, filename):
         with open(os.path.join(Publisher.SOURCE_FOLDER, filename), 'rb') as f:
             filepayload = f.read()
             self._message['imageString'] = base64.b64encode(filepayload).decode()
 
-    def _attach_to_one_topic(self, topic):
-        self._client.publish(topic, json.dumps(self._message), self._qos)
-
     def _attach_to_topics(self, topics):
         for topic in topics:
             self._attach_to_one_topic(topic)
+
+    def _attach_to_one_topic(self, topic):
+        self._client.publish(topic, json.dumps(self._message), self._qos)
 
--- debug/vehicle/subscribe/subscriber.py
+++ debug/vehicle/subscribe/subscriber.py
@@ -3,10 +3,9 @@ import json
 import base64
 import os
 
-# class for subscriber
 class Subscriber(object):
 
-    # constant variable
+    # the folder where received files go to
     OUTPUT_FOLDER = '../output/'
 
     def __init__(self):
@@ -15,28 +14,11 @@ class Subscriber(object):
         self._message = {}
         self._client.on_connect = self._on_connect
 
-    def connect_broker(self, broker):
-        self._client.connect(broker.get_url(), broker.get_port(), 1800)
-
     def _on_connect(self, client, userdata, flags, rc):
         print('connected with result code', rc)
 
-    def _on_receive_data(self, client, userdata, message):
-        self._extract_message(message)
-        self._recover_image()
-        self._print_name_of_received_file()
-    
-    def _recover_image(self):
-        filename = self._message['filename']
-        with open(os.path.join(Subscriber.OUTPUT_FOLDER, filename), 'wb') as f:
-            f.write(base64.b64decode(self._message['imageString']))
-
-    def _print_name_of_received_file(self):
-        print(self._message['filename'])
-
-    def _extract_message(self, message):
-        decoded = str(message.payload.decode("utf-8", "ignore"))
-        self._message = json.loads(decoded)
+    def connect_broker(self, broker):
+        self._client.connect(broker.get_url(), broker.get_port(), 1800)
 
     def subscribe_to_topics(self, topics):
         for topic in topics:
@@ -47,4 +29,25 @@ class Subscriber(object):
     def _subscribe_to_one_topic(self, topic):
         self._client.subscribe(topic, self._qos)
         self._client.message_callback_add(topic, self._on_receive_data)
+
+    def _on_receive_data(self, client, userdata, message):
+        self._extract_message(message)
+        self._recover_received_file()
+        self._print_name_of_received_file()
+
+    def _extract_message(self, message):
+        decoded = str(message.payload.decode("utf-8", "ignore"))
+        self._message = json.loads(decoded)
+   
+    def _recover_received_file(self):
+        filename = self._message['filename']
+        self._write_to_file(filename)
+
+    def _write_to_file(self, filename):
+        with open(os.path.join(Subscriber.OUTPUT_FOLDER, filename), 'wb') as f:
+            f.write(base64.b64decode(self._message['imageString']))
+
+    def _print_name_of_received_file(self):
+        print(self._message['filename'])
+
 
